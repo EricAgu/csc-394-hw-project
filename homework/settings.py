8cbbee5e55401b9ae33e45644da388909db6dac5
EricAgu <97047505+EricAgu@users.noreply.github.com>
2023-04-20 15:43:40 -0500

    Postgresql update

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s)5bnm!n)xg#!8*3^q(z9vcs*w03y&7s#9x=rgo#c0lr^t(!c9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'computer', 
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
